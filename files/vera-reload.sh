93a1ae99021ef26dc9b7cefccda6c18703e440a1
Jonathan Freedman <jonafree@gmail.com>
2016-06-13 23:17:14 -0700

    better handling of reloading vera plugins

--- files/vera-reload.sh
+++ files/vera-reload.sh
@@ -2,12 +2,16 @@
 
 function reload {
     cd "${DIR}"
-    lua *.lua || exit 1 
-    xmllint *.xml || exit 1
-    for f in D_*.json ; do
-        json_pp < $f || exit 1
-    done
-    scp *.xml *.json *.lua vera:/etc/cmh-ludl/ && \
+    lua *.lua || exit 1
+    if ls *.xml &> /dev/null ; then
+        xmllint *.xml || exit 1
+    fi
+    if ls D_*.json &> /dev/null ; then
+        for f in D_*.json ; do
+            json_pp < $f || exit 1
+        done
+    fi
+    scp * vera:/etc/cmh-ludl/ && \
         curl 'http://vera:3480/data_request?id=reload' || exit 1
 }
 
@@ -18,13 +22,16 @@ else
 fi
 RC=0
 OWD=$(pwd)
+if [ -d "${DIR}/plugin" ] ; then
+    DIR="${DIR}/plugin"
+fi
 if ! ls $DIR/*.lua &> /dev/null ; then
     echo "no lua files in ${DIR}"
     exit 1
 fi
 
 while [ true ] ; do
-    dialog --yesno "Deploy that sweet\nsweet\nlua\n\n?" 10 30
+    dialog --yesno "${DIR}\nDeploy that sweet\nsweet\nlua\n\n?" 10 30
     if [ $? == 0 ] ; then
         reload
     else
